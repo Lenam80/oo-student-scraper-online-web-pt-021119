e6e880de9ada41bc6be45112f588e22f603912cc
Angelina Montanez <angelinamontanez80@gmail.com>
2019-04-03 17:36:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,14 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+attr_accessors: student: name:
 
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
